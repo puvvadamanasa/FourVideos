3f7d126ebdf759873c108c643f2f9aee6ed4f2cf
puvvadamanasa <puvvadamanasa0@gmail.com>
2023-07-14 16:24:35 +0530

    update orderpage

--- src/main/java/four/videos/pages/OrderPage.java
+++ src/main/java/four/videos/pages/OrderPage.java
@@ -5,5 +5,11 @@ import org.openqa.selenium.By;
 public class OrderPage {
 	By loc = By.id("order");
 	By hai = By.id("hau");
+	public void hai() {
+		System.out.println("haia");
+	}
+	public void hello() {
+		System.out.println("hello");
+	}
 
 }
